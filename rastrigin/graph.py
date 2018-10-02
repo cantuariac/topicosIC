10dcfeef44879adae04adb39d29ff8533c64187d
cantuariac <caio.cantuaria@gmail.com>
2018-10-02 08:32:22 -0300

    Atividade Concluía Minimizar Rastrigin

--- rastrigin/graph.py
+++ rastrigin/graph.py
@@ -9,15 +9,14 @@ def rosen(parametros):
     """função Rosenbrock"""
     return 100.0*(x1-x2**2)**2.0 + (x2-1)**2
 
-def rastrigin(X, A=10):
-	#A = kwargs.get('A', 10)
-	return A + sum([(x**2 - A * np.cos(2 * math.pi * x)) for x in X])
+def rastrigin(X):
+	return 10*len(X) + sum([(x**2 - 10 * np.cos(2 * math.pi * x)) for x in X])
 
 X = np.linspace(-4, 4, 200)    
 Y = np.linspace(-4, 4, 200) 
 X, Y = np.meshgrid(X, Y)
 
-Z = rastrigin([X, Y], A=10)
+Z = rastrigin([X, Y])
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
